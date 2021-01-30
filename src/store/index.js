584433ec1cf7d92b9388bde0370efcc096149b22
Jonathan Hinkel <hinkeljonathan@protonmail.com>
2021-01-30 20:09:17 +0100

    fixed material requirement key mismatch

--- src/store/index.js
+++ src/store/index.js
@@ -67,23 +67,23 @@ export default new Vuex.Store({
       {xp: 3424125, mora: 684600}
     ], 
     ascensionTable: [
-      {elementalMat_T1: 1, elementalMat_T2: 0, elementalMat_T3: 0, elementalMat_T4: 0, core: 0, speciality: 3, commonMat_T1: 3, commonMat_T2: 0, commonMat_T3: 0, mora: 20000}, 
-      {elementalMat_T1: 0, elementalMat_T2: 3, elementalMat_T3: 0, elementalMat_T4: 0, core: 2, speciality: 10, commonMat_T1: 15, commonMat_T2: 0, commonMat_T3: 0, mora: 40000}, 
-      {elementalMat_T1: 0, elementalMat_T2: 6, elementalMat_T3: 0, elementalMat_T4: 0, core: 4, speciality: 20, commonMat_T1: 0, commonMat_T2: 12, commonMat_T3: 0, mora: 60000}, 
-      {elementalMat_T1: 0, elementalMat_T2: 0, elementalMat_T3: 3, elementalMat_T4: 0, core: 8, speciality: 30, commonMat_T1: 0, commonMat_T2: 18, commonMat_T3: 0, mora: 80000},
-      {elementalMat_T1: 0, elementalMat_T2: 0, elementalMat_T3: 6, elementalMat_T4: 0, core: 12, speciality: 45, commonMat_T1: 0, commonMat_T2: 0, commonMat_T3: 12, mora: 100000},
-      {elementalMat_T1: 0, elementalMat_T2: 0, elementalMat_T3: 0, elementalMat_T4: 6, core: 20, speciality: 60, commonMat_T1: 0, commonMat_T2: 0, commonMat_T3: 24, mora: 120000}  
+      {elementalMat_T1: 1, elementalMat_T2: 0, elementalMat_T3: 0, elementalMat_T4: 0, core: 0, speciality: 3, commonMat_Ascension_T1: 3, commonMat_Ascension_T2: 0, commonMat_Ascension_T3: 0, mora: 20000}, 
+      {elementalMat_T1: 0, elementalMat_T2: 3, elementalMat_T3: 0, elementalMat_T4: 0, core: 2, speciality: 10, commonMat_Ascension_T1: 15, commonMat_Ascension_T2: 0, commonMat_Ascension_T3: 0, mora: 40000}, 
+      {elementalMat_T1: 0, elementalMat_T2: 6, elementalMat_T3: 0, elementalMat_T4: 0, core: 4, speciality: 20, commonMat_Ascension_T1: 0, commonMat_Ascension_T2: 12, commonMat_Ascension_T3: 0, mora: 60000}, 
+      {elementalMat_T1: 0, elementalMat_T2: 0, elementalMat_T3: 3, elementalMat_T4: 0, core: 8, speciality: 30, commonMat_Ascension_T1: 0, commonMat_Ascension_T2: 18, commonMat_Ascension_T3: 0, mora: 80000},
+      {elementalMat_T1: 0, elementalMat_T2: 0, elementalMat_T3: 6, elementalMat_T4: 0, core: 12, speciality: 45, commonMat_Ascension_T1: 0, commonMat_Ascension_T2: 0, commonMat_Ascension_T3: 16, mora: 100000},
+      {elementalMat_T1: 0, elementalMat_T2: 0, elementalMat_T3: 0, elementalMat_T4: 6, core: 20, speciality: 60, commonMat_Ascension_T1: 0, commonMat_Ascension_T2: 0, commonMat_Ascension_T3: 24, mora: 120000}  
     ], 
     talentTable: [
-      {book_T1: 3, book_T2: 0, book_T3: 0, commonMat_T1: 6, commonMat_T2: 0, commonMat_T3: 0, worldBossMat: 0, crown: 0, mora: 12500},
-      {book_T1: 0, book_T2: 2, book_T3: 0, commonMat_T1: 0, commonMat_T2: 3, commonMat_T3: 0, worldBossMat: 0, crown: 0, mora: 17500},
-      {book_T1: 0, book_T2: 4, book_T3: 0, commonMat_T1: 0, commonMat_T2: 4, commonMat_T3: 0, worldBossMat: 0, crown: 0, mora: 25000},
-      {book_T1: 0, book_T2: 6, book_T3: 0, commonMat_T1: 0, commonMat_T2: 6, commonMat_T3: 0, worldBossMat: 0, crown: 0, mora: 30000},
-      {book_T1: 0, book_T2: 9, book_T3: 0, commonMat_T1: 0, commonMat_T2: 9, commonMat_T3: 0, worldBossMat: 0, crown: 0, mora: 37500},
-      {book_T1: 0, book_T2: 0, book_T3: 4, commonMat_T1: 0, commonMat_T2: 0, commonMat_T3: 4, worldBossMat: 1, crown: 0, mora: 120000},
-      {book_T1: 0, book_T2: 0, book_T3: 6, commonMat_T1: 0, commonMat_T2: 0, commonMat_T3: 6, worldBossMat: 1, crown: 0, mora: 260000},
-      {book_T1: 0, book_T2: 0, book_T3: 12, commonMat_T1: 0, commonMat_T2: 0, commonMat_T3: 9, worldBossMat: 2, crown: 0, mora: 450000},
-      {book_T1: 0, book_T2: 0, book_T3: 16, commonMat_T1: 0, commonMat_T2: 0, commonMat_T3: 12, worldBossMat: 2, crown: 1, mora: 700000}
+      {book_T1: 3, book_T2: 0, book_T3: 0, commonMat_Talent_T1: 6, commonMat_Talent_T2: 0, commonMat_Talent_T3: 0, worldBossMat: 0, crown: 0, mora: 12500},
+      {book_T1: 0, book_T2: 2, book_T3: 0, commonMat_Talent_T1: 0, commonMat_Talent_T2: 3, commonMat_Talent_T3: 0, worldBossMat: 0, crown: 0, mora: 17500},
+      {book_T1: 0, book_T2: 4, book_T3: 0, commonMat_Talent_T1: 0, commonMat_Talent_T2: 4, commonMat_Talent_T3: 0, worldBossMat: 0, crown: 0, mora: 25000},
+      {book_T1: 0, book_T2: 6, book_T3: 0, commonMat_Talent_T1: 0, commonMat_Talent_T2: 6, commonMat_Talent_T3: 0, worldBossMat: 0, crown: 0, mora: 30000},
+      {book_T1: 0, book_T2: 9, book_T3: 0, commonMat_Talent_T1: 0, commonMat_Talent_T2: 9, commonMat_Talent_T3: 0, worldBossMat: 0, crown: 0, mora: 37500},
+      {book_T1: 0, book_T2: 0, book_T3: 4, commonMat_Talent_T1: 0, commonMat_Talent_T2: 0, commonMat_Talent_T3: 4, worldBossMat: 1, crown: 0, mora: 120000},
+      {book_T1: 0, book_T2: 0, book_T3: 6, commonMat_Talent_T1: 0, commonMat_Talent_T2: 0, commonMat_Talent_T3: 6, worldBossMat: 1, crown: 0, mora: 260000},
+      {book_T1: 0, book_T2: 0, book_T3: 12, commonMat_Talent_T1: 0, commonMat_Talent_T2: 0, commonMat_Talent_T3: 9, worldBossMat: 2, crown: 0, mora: 450000},
+      {book_T1: 0, book_T2: 0, book_T3: 16, commonMat_Talent_T1: 0, commonMat_Talent_T2: 0, commonMat_Talent_T3: 12, worldBossMat: 2, crown: 1, mora: 700000}
     ], 
     xpMaterials: {
       character: {name: 'Hero’s Wit', amount: 20000}
